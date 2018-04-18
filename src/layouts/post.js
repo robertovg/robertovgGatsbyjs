9658c0facec5273f3d7e23e4dadff6d9f15f031c
Roberto Vázquez González <robertovg@precursive.co.uk>
2018-04-18 17:07:22 +0200

    trying to append discuss just to the body

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -36,7 +36,7 @@ export default function Template({ data }) {
             var d = document, s = d.createElement('script');
             s.src = 'https://robertovg-com.disqus.com/embed.js';
             s.setAttribute('data-timestamp', +new Date());
-            (d.head || d.body).appendChild(s);
+            d.body.appendChild(s);
             })();
         `,
         }}
